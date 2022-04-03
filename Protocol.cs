0a0ce472cd3976e19ddfbaefa0f6eda3bf3f9b0a
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2022-04-03 18:12:55 +0100

    Deflate compression for frames and more commands

--- Protocol.cs
+++ Protocol.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.IO.Compression;
 using System.Collections.Generic;
@@ -10,40 +10,51 @@ namespace waninput2
 {
     class Protocol
     {
-        public static byte HANDSHAKE = 1;
-        public static byte CONTROL = 2;
-        public static byte FRAME = 3;
+        public static byte CONNECT = 1;
+        public static byte DISCONNECT = 1;
+        public static byte FRAME = 2;
+        public static byte CONTROL = 3;
 
-        public static byte[] Datagram(byte command, int data)
+        public static byte BUTTON = 4;
+        public static byte AXIS = 5;
+        public static byte HAT = 6;
+
+        public static byte[] Datagram(byte command, byte[] data)
         {
-            byte[] bytes = new byte[32];
+            List<byte> d = new List<byte>(data);
+            d.Insert(0, command);
+            Console.WriteLine(string.Join(' ', d));
+            return d.ToArray();
+        }
 
-            bytes[0] = command;
-
-            int idx = 1;
-            foreach (byte b in BitConverter.GetBytes(data))
-            {
-                bytes[idx] = b;
-                idx++;
-            }
-
-            Console.WriteLine(string.Join(' ', bytes));
-            return bytes;
+        public static byte[] Datagram(byte command, byte subcommand, byte[] data)
+        {
+            List<byte> d = new List<byte>(data);
+            d.Insert(0, subcommand);
+            d.Insert(0, command);
+            Console.WriteLine(string.Join(' ', d));
+            return d.ToArray();
         }
 
         public static byte[] Encode(Bitmap b)
         {
-            using MemoryStream inp = new MemoryStream();
-            b.Save(inp, ImageFormat.Bmp);            
+            using MemoryStream temp = new MemoryStream();
+            using (DeflateStream ds = new DeflateStream(temp, CompressionMode.Compress))
+            {
+                b.Save(ds, ImageFormat.Jpeg);
+            }
+
             b.Dispose();
 
-            return inp.ToArray();
+            return temp.ToArray();
         }
 
         public static Bitmap Decode(byte[] s)
         {
             using MemoryStream m = new MemoryStream();
-            m.Write(s);
+            using MemoryStream temp = new MemoryStream(s);
+            using DeflateStream ds = new DeflateStream(temp, CompressionMode.Decompress);
+            ds.CopyTo(m);
 
             return new Bitmap(m);
         }
@@ -53,11 +64,12 @@ namespace waninput2
             Rectangle r = new Rectangle(0, 0, width, height);
             Bitmap scaled = new Bitmap(width, height, PixelFormat.Format24bppRgb);
 
-            using (Graphics g2 = Graphics.FromImage(scaled))
+            using (Graphics g = Graphics.FromImage(scaled))
             {
-                g2.InterpolationMode = InterpolationMode.High;
-                g2.SmoothingMode = SmoothingMode.HighQuality;
-                g2.DrawImage(b, r, 0, 0, b.Width, b.Height, GraphicsUnit.Pixel);
+                g.CompositingQuality = CompositingQuality.HighSpeed;
+                g.InterpolationMode = InterpolationMode.Bilinear;
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                g.DrawImage(b, r, 0, 0, b.Width, b.Height, GraphicsUnit.Pixel);
             }
 
             return scaled;
